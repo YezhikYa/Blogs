31f801af2de51f5b0ed25e9a05ccb1bf239b911d
User <nr6890@pb.amalnet.k12.il>
2023-12-21 09:59:11 +0200

    mf

--- REPOSITORY/src/main/java/com/example/repositwory/BlogsRepository.java
+++ REPOSITORY/src/main/java/com/example/repositwory/BlogsRepository.java
@@ -1,5 +1,7 @@
 package com.example.repositwory;
 
+import android.content.Context;
+
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -7,9 +9,23 @@ public class BlogsRepository {
     private FirebaseFirestore db;
     private CollectionReference collection;
 
-    public BlogsRepository()
-        {
+    public BlogsRepository() {
         db = FirebaseFirestore.getInstance();
         collection = db.collection("Blogs");
+    }
+
+    public BlogsRepository(Context context) {
+        try {
+            db = FirebaseFirestore.getInstance();
+        } catch (Exception e) {
+            FirebaseInstance instance =
+                    FirebaseInstance.instance(context);
+
+            db = FirebaseFirestore
+                    .getInstance(FirebaseInstance.app);
+        }
+
+        collection = db.collection("Blogs");
+
     }
 }
--- VIEWMODEL/src/main/java/com/yezhik_ya/viewmodel/BlogsViewModel.java
+++ VIEWMODEL/src/main/java/com/yezhik_ya/viewmodel/BlogsViewModel.java
@@ -1,18 +1,34 @@
 package com.yezhik_ya.viewmodel;
 
+import android.content.Context;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.repositwory.BlogsRepository;
 import com.yezhik_ya.model.BlogPost;
+import com.yezhik_ya.model.BlogPosts;
 
 public class BlogsViewModel extends ViewModel
 {
     private MutableLiveData<Boolean> successOperation;
+    private BlogsRepository repository;
+    private MutableLiveData<BlogPosts> blogsPostsLiveData;
 
     public BlogsViewModel() { successOperation = new MutableLiveData<>(); }
+    public BlogsViewModel(Context context){
+        successOperation = new MutableLiveData<>();
+        repository = new BlogsRepository(context);
+        blogsPostsLiveData = new MutableLiveData<>();
+    }
 
-    public void add(BlogPost blogPost) { successOperation.setValue(true); }
+
+    public void add(BlogPost blogPosts) {
+        repository.add(blogPost)
+                .addOnSuccessListener(aBoolean -> {successOperation.setValue(true);})
+                .addOnFailureListener(e ->{successOperation.setValue(false);});
+    }
 
     public LiveData<Boolean> getSuccessOperation() { return successOperation; }
 
